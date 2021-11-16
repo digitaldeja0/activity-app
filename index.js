8af4ec3ccbb4313bb7921b88989f6d9743e11313
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2021-11-15 23:53:50 -0500

    Update index.js
    Update api address.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 document.getElementById('btn').addEventListener("click", function(){
-  fetch("http://www.boredapi.com/api/activity/")
+  fetch("https://apis.scrimba.com/bored/api/activity")
       .then(response => response.json())
       .then(data => {
         document.getElementById('bored-api-text').innerHTML = `
